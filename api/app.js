885452e04216bc961c3483c6a92edaf0e824ba61
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2024-12-14 21:33:52 -0500

    created auth/register - prisma - mongo db

--- api/app.js
+++ api/app.js
@@ -3,8 +3,9 @@ const app = express();
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 
+app.use(express.json());
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
 app.listen(8800, () => {
-  console.log("server running");
+  console.log("server rsrunning");
 });
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,30 @@
-export const register = (req, res) => {
-  console.log("meaw");
+import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  //   Hash Passwod
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+    // create a user and save it to the database
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+    console.log(newUser);
+    res.status(201).json({ message: "user created successufully" });
+  } catch (err) {
+    console.log(Error);
+    res.status(500).json({ message: "failed to create user" });
+  }
 };
-export const login = (req, res) => {
-  console.log("login");
-};
+
+export const login = async (req, res) => {};
+
 export const logout = (req, res) => {
   console.log("logout");
 };
